777b263c96860c239529c0dfc46b92f9b150b94b
Yevhen <96913622+Ev93n1Y@users.noreply.github.com>
2023-07-14 00:17:37 +0300

    add servlet initializer

---
+++ src/main/java/ua/teamchallenge/onlineShop/ServletInitializer.java
@@ -0,0 +1,12 @@
+package ua.teamchallenge.onlineShop;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(OnlineShopApplication.class);
+	}
+}
