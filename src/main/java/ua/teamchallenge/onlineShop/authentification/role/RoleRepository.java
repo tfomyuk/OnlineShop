255dd4ebc74ec957571da1911ccc20925d364295
Yevhen <96913622+Ev93n1Y@users.noreply.github.com>
2023-07-14 00:19:02 +0300

    add auth repositories

---
+++ src/main/java/ua/teamchallenge/onlineShop/authentification/role/RoleRepository.java
@@ -0,0 +1,9 @@
+package ua.teamchallenge.onlineShop.authentification.role;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.UUID;
+
+public interface RoleRepository extends JpaRepository<Role, UUID> {
+    Role findByName(String name);
+}
