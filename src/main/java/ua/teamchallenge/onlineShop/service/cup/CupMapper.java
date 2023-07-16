c19bed2ca1808dbc80dc9c21646f41cdcb3ab391
Yevhen <96913622+Ev93n1Y@users.noreply.github.com>
2023-07-16 16:26:43 +0300

    add Cup entities mapper

---
+++ src/main/java/ua/teamchallenge/onlineShop/service/cup/CupMapper.java
@@ -0,0 +1,41 @@
+package ua.teamchallenge.onlineShop.service.cup;
+
+import org.springframework.stereotype.Service;
+import ua.teamchallenge.onlineShop.service.EntityMapper;
+
+@Service
+public class CupMapper implements EntityMapper<CupDto, Cup> {
+    @Override
+    public CupDto mapToDto(Cup cup) {
+        return new CupDto(
+                cup.getId(),
+                cup.getPrize(),
+                cup.getType(),
+                cup.getKind(),
+                cup.getColor(),
+                cup.getSurface(),
+                cup.getShape(),
+                cup.getMaterial(),
+                cup.getCapacity(),
+                cup.getDiameter(),
+                cup.getDescription()
+        );
+    }
+
+@Override
+    public Cup mapToDao(CupDto cupDto){
+        return new Cup(
+                cupDto.getId(),
+                cupDto.getPrize(),
+                cupDto.getType(),
+                cupDto.getKind(),
+                cupDto.getColor(),
+                cupDto.getSurface(),
+                cupDto.getShape(),
+                cupDto.getMaterial(),
+                cupDto.getCapacity(),
+                cupDto.getDiameter(),
+                cupDto.getDescription()
+        );
+    }
+}
