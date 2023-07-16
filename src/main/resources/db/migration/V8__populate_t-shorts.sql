465f049d9466b9d8358c41dd7d234ad3a7735213
Yevhen <96913622+Ev93n1Y@users.noreply.github.com>
2023-07-16 20:33:38 +0300

    populate test data to t-shorts

---
+++ src/main/resources/db/migration/V8__populate_t-shorts.sql
@@ -0,0 +1,6 @@
+INSERT INTO tshorts (id, prize, material, density, style, size, quantity, temperature, color, image, description)
+VALUES
+('fcf12a8d-91e9-4bb6-b60b-53d6d97a9f44', 250, '100% бавовна кільцевого прядіння; GYH - 85% бавовна,15% віскоза',155,
+ 'Casual Fit', '2XS', '96 / коробка (2XS-XS 48; 3-5XL 24)', 40, 'Білий', 'http:\\img.server.com\some-image.jpg',
+ 'Детальний опис1');
+
