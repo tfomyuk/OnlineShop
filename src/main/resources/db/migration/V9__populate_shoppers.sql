66ec9df148f0be38d6c4eb592812163936706ad8
Yevhen <96913622+Ev93n1Y@users.noreply.github.com>
2023-07-16 20:33:55 +0300

    populate test data to shoppers

---
+++ src/main/resources/db/migration/V9__populate_shoppers.sql
@@ -0,0 +1,5 @@
+INSERT INTO shoppers (id, prize, size, textile, handles_length, quantity, image, description)
+VALUES
+('fcf12a8d-91e9-4bb6-b60b-53d6d97a9f44', 250, '35x41', 'невибілена бавовна , бязь, 135 г/кв.м',
+ 60, 50, 'http:\\img.server.com\some-image.jpg', 'Детальний опис1');
+
