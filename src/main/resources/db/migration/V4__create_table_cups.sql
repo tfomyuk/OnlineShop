628b1287f65ff0d147256c7bd0829ae71c698072
Yevhen <96913622+Ev93n1Y@users.noreply.github.com>
2023-07-16 16:22:03 +0300

    create table cups

---
+++ src/main/resources/db/migration/V4__create_table_cups.sql
@@ -0,0 +1,13 @@
+CREATE TABLE cups (
+    id          UUID            NOT NULL PRIMARY KEY,
+    prize       INT,
+    type        VARCHAR(255),
+    kind        VARCHAR(255),
+    color       VARCHAR(255),
+    surface     VARCHAR(255),
+    shape       VARCHAR(255),
+    material    VARCHAR(255),
+    capacity    INT,
+    diameter    INT,
+    description VARCHAR(255)
+);
