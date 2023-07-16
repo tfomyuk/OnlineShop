a57348e918bb39e6630dec5e8b6c690b9c4c55a7
Yevhen <96913622+Ev93n1Y@users.noreply.github.com>
2023-07-16 16:35:09 +0300

    OpenAPIDefinition url = Default Server URL

--- src/main/java/ua/teamchallenge/onlineShop/OnlineShopApplication.java
+++ src/main/java/ua/teamchallenge/onlineShop/OnlineShopApplication.java
@@ -1,9 +1,12 @@
 package ua.teamchallenge.onlineShop;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.servers.Server;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication()
+@OpenAPIDefinition(servers = {@Server(url = "/", description = " ")})
 public class OnlineShopApplication {
 
 	public static void main(String[] args) {
